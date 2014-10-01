17f4dc87931107da91f79f26cdf263755eb29b0a
ct2034 <post@henkelchristian.de>
2014-10-01 14:47:08 +1300

    including license info in sourcefiles

--- auckbot_gazebo/src/current_pub.cpp
+++ auckbot_gazebo/src/current_pub.cpp
@@ -1,3 +1,19 @@
+/*************************************************
+current_pub.cpp                          
+---------------
+                                              
+Copyright 2014 Christian Henkel
+Contact: post@henkelchristian.de 
+   
+This file is part of Auckbot.
+
+Auckbot is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+Auckbot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Auckbot. If not, see http://www.gnu.org/licenses/.             
+***************************************************/
+
 #include "ros/ros.h"
 #include "auckbot_gazebo/MotorCurrents.h"
 #include "geometry_msgs/Twist.h"
