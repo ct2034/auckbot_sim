49f634351a7024c8f668195b9ed91b6e0bf23509
ct2034 <post@henkelchristian.de>
2014-11-30 14:10:27 +1300

    adding NavFN option

--- auckbot_navigation/launch/planner_setup_selector.sh
+++ auckbot_navigation/launch/planner_setup_selector.sh
@@ -2,26 +2,32 @@
 # planner setup selector
 
 PS3='Please enter your choice: '
-options=("Carrot Planner" "Global Planner Grid" "Global Planner No Grid")
+options=("NavFN" "Carrot Planner" "Global Planner Grid" "Global Planner No Grid")
 select opt in "${options[@]}"
 do
     case $opt in
+        "NavFN")
+            export MB_BASE_GLOBAL_PLANNER='navfn/NavfnROS'
+            export MB_USE_GRID_PATH='N/A'
+            echo 'planner: $MB_BASE_GLOBAL_PLANNER, grid: $MB_USE_GRID_PATH'
+            break
+            ;;
         "Carrot Planner")
-			export MB_BASE_GLOBAL_PLANNER='carrot_planner/CarrotPlanner'
-			export MB_USE_GRID_PATH='N/A'
-			echo 'planner: carrot_planner/CarrotPlanner, grid: N/A'
+            export MB_BASE_GLOBAL_PLANNER='carrot_planner/CarrotPlanner'
+            export MB_USE_GRID_PATH='N/A'
+            echo 'planner: $MB_BASE_GLOBAL_PLANNER, grid: $MB_USE_GRID_PATH'
             break
             ;;
         "Global Planner Grid")
 			export MB_BASE_GLOBAL_PLANNER='global_planner/GlobalPlanner'
 			export MB_USE_GRID_PATH='true'
-			echo 'planner: global_planner/GlobalPlanner, grid: true'
+            echo 'planner: $MB_BASE_GLOBAL_PLANNER, grid: $MB_USE_GRID_PATH'
             break
             ;;
         "Global Planner No Grid")
 			export MB_BASE_GLOBAL_PLANNER='global_planner/GlobalPlanner'
 			export MB_USE_GRID_PATH='false'
-			echo 'planner: global_planner/GlobalPlanner, grid: false'
+            echo 'planner: $MB_BASE_GLOBAL_PLANNER, grid: $MB_USE_GRID_PATH'
             break
             ;;
         *) echo invalid option;;
